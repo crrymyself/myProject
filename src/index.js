56e3e7f81ae831b6681ed99a6fec97a38e271f1c
24 <462501908@qq.com>
2019-09-06 15:44:46 +0800

    2019-9-6 di五次

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ import './index.less';
 // 2019/9/5测试是否打包 第二次
 // 2019/9/5测试是否打包 第三次
 // 2019/9/6测试是否打包 第四次！！！
+// 2019/9/6测试是否打包 第五次！！！
 // 1. Initialize
 const app = dva({
   history: createHistory(),
